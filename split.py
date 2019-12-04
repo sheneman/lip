1cf3a9cb1837fe769d4222337277064be1a0a1bf
sheneman <sheneman@uidaho.edu>
2019-12-04 08:58:49 -0800

    split into train, validation, test:

--- split.py
+++ split.py
@@ -8,6 +8,11 @@ from time import time
 from random import seed, random, shuffle
 from math import floor
 
+#
+# This script takes a directory of input files and partitions them into a list of
+# input files to use for TRAINING, VALIDATION, and TESTING 
+#
+
 
 # set our random seed based on current time
 now = int(time())
@@ -18,13 +23,17 @@ IMG_RAWPATH = "../images/raw"
 IMG_BINPATH = "../images/binary"
 
 # Set output filenames
-TRAINSET_FILENAME = "train.txt"
-TESTSET_FILENAME = "test.txt"
+TRAIN_FILENAME      = "train_list.txt"
+VALIDATION_FILENAME = "validation_list.txt"
+TEST_FILENAME    = "test_list.txt"
 
 FILE_FILTER = "*.tif"
 
-# The fraction of the image library that will be used for training
-TRAIN_FRACTION = 0.75
+# The fraction of the image library that will be used for training, validation, and testing
+# The totals must add up to 1.0
+TRAIN_FRACTION      = 0.3333333
+VALIDATION_FRACTION = 0.3333333
+TEST_FRACTION       = 0.3333333
 
 # Get all of the filenames that match the filter and shuffle them in place
 cwd = os.getcwd()
@@ -41,11 +50,16 @@ if(TRAIN_FRACTION < 0 or TRAIN_FRACTION > 1.0):
 
 train_partition_start = 0
 train_partition_end = floor(num_filenames*TRAIN_FRACTION-1)
+validation_partition_size = floor(num_filenames*VALIDATION_FRACTION)
 if(train_partition_end == num_filenames-1):
 	test_partition_start = -1
 	test_partition_end   = -1
+	validation_partition_start = -1
+	validation_partition_end = -1
 else:
-	test_partition_start = train_partition_end+1
+	validation_partition_start = train_partition_end+1
+	validation_partition_end = validation_partition_start + validation_partition_size
+	test_partition_start = validation_partition_end + 1
 	test_partition_end   = num_filenames-1
 
 if(train_partition_start < 0 or train_partition_end < 0 or train_partition_end < train_partition_start):
@@ -55,21 +69,30 @@ if(train_partition_start < 0 or train_partition_end < 0 or train_partition_end <
 
 print("TOTAL NUMBER OF FILENAMES: %d" %num_filenames)
 print("TRAIN: %d thru %d" %(train_partition_start,train_partition_end))
+print("VALIDATION: %d thru %d" %(validation_partition_start,validation_partition_end))
 print("TEST: %d thru %d" %(test_partition_start,test_partition_end))
+
 
 # Write the training set input file
 if(train_partition_start >= 0):
-	train_file = open(TRAINSET_FILENAME, "w")
+	file = open(TRAIN_FILENAME, "w")
 	for i in range(train_partition_start, train_partition_end+1):
-		train_file.write(filenames[i] + "\n")
-	train_file.close()
+		file.write(filenames[i] + "\n")
+	file.close()
+
+# Write the validation set input file
+if(validation_partition_start >= 0):
+	file = open(VALIDATION_FILENAME, "w")
+	for i in range(train_partition_start, train_partition_end+1):
+		file.write(filenames[i] + "\n")
+	file.close()
 
 # Write the test set input file
 if(test_partition_start >= 0):
-	test_file = open(TESTSET_FILENAME, "w")
+	file = open(TEST_FILENAME, "w")
 	for i in range(test_partition_start, test_partition_end+1):
-		test_file.write(filenames[i] + "\n")
-	test_file.close()
+		file.write(filenames[i] + "\n")
+	file.close()
 
 exit(0)
 
